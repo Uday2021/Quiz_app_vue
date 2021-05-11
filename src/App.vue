b2b15efcf2bfc577db997a9c4eef374bbd266bbe
Uday <udaysaraswat2@gmail.com>
2021-05-11 17:24:42 +0530

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+ <div id="app">
+  <sign-up />
+ </div>
+</template>
+
+<script>
+import signUp from './components/signUp.vue';
+export default {
+  name: 'App',
+  components:{
+      signUp,
+  },
+
+}
+</script>
+
+<style>
+
+#app{
+  font-family: Avenir,Arial, Helvetica, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+
+</style>
---
+++ src/components/signUp.vue
@@ -0,0 +1,46 @@
+<template>
+<div>
+<img class="logo" src="../assets/quiz.jpg"/>
+<h1> SignUp </h1>
+<div class = "register">
+    <input type="text" placeholder="Enter Name"/>
+    <input type="email" placeholder="Enter Email"/>
+    <input type="password" placeholder="Enter Password"/>
+    <button>SignUp</button>
+
+</div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'signUp'
+}
+</script>
+
+<style>
+.logo{
+    width: 150px;
+}
+
+.register input{
+    width: 300px;
+    height: 40px;
+    padding-left: 20px;
+    display: block;
+    margin-bottom: 30px;
+    margin-left: auto;
+    margin-right: auto;
+    border: 1px solid orange;
+}
+
+.register button{
+    width: 320px;
+    height: 40px;
+    border: 1px solid orange;
+    color: #fff;
+    background-color: orange;
+    cursor: pointer;
+    border-radius: 4px;
+}
+</style>
